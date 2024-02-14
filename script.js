27f0778282af1caeae57cff2b6ed437c73f388cc
Reshuv <RESHU0899@GMAIL.COM>
2024-02-15 02:15:49 +0530

    Added city , humidity and temp

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 async function fetchWeatherData(cityName){
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=d954360f7eb2efceeacbef45f36d06c9`;
     const response = await fetch(url);
-    const data = await response.json(response);
+    const data = await response.json();
+    let city = document.getElementById("city");
+    city.innerHTML=data.name;
+    let windSpeed = document.getElementById("wind_speed");
+    windSpeed.innerHTML=data.wind.speed+" km/hr ";
+    let humidity=document.getElementById("humidity");
+    humidity.innerHTML=data.main.humidity+ "g/kg";
+    let temp = document.getElementById("temp");
+    temp.innerHTML = Math.round((data.main.temp)-273.15)+"°C";
     console.log(data);
 }
 //fetchWeatherData()
